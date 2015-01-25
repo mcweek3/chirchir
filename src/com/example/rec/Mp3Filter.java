81490dfc49d78c487f1f112f7670e9009e762aa9
storykim <shanytt@gmail.com>
2015-01-25 15:56:15 +0900

    delete things

--- src/com/example/rec/Mp3Filter.java
+++
@@ -1,9 +0,0 @@
-package com.example.rec;
-
-import java.io.File;
-import java.io.FilenameFilter;
-
-public class Mp3Filter implements FilenameFilter {     
-	public boolean accept(File dir, String name) {         
-		return (name.endsWith(".mp4"));     
-		}}
--- src/com/example/rec/s.java
+++
@@ -1,5 +0,0 @@
-package com.example.rec;
-
-public class s {
-
-}
